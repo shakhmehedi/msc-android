fdaa08ce5d997776f2ae3153107de233d692037c
Shakh Mehedi <shakhmehedi@yahoo.com>
2018-04-16 13:16:28 +0100

    Replaced: Removed fab button from all activity

--- app/src/main/java/uk/ac/uws/msc/shakh/ProductListActivity.java
+++ app/src/main/java/uk/ac/uws/msc/shakh/ProductListActivity.java
@@ -51,15 +51,6 @@ public class ProductListActivity extends AppCompatActivity implements Navigation
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         mProductRecyclerAdapter = new ProductRecyclerAdapter(this);
 
         mIntent = getIntent();
